50447b7558076f43a29def44fe85baa3953b48a3
Gel <gel@leandro@alertaapp.com.br>
2018-09-16 13:53:32 -0300

    add password to user

--- tests/test_user_api.py
+++ tests/test_user_api.py
@@ -1,9 +1,10 @@
-from petshop.user_model import get_user
+from petshop.user_model import get_user, insert_user
 
 
 def test_user_insert(client, app):
     payload = {
             'username': 'batima',
+            'password': '123456',
             'email': 'teste@teste.com'
             }
     res = client.post('/user', data=payload)
@@ -16,3 +17,19 @@ def test_user_insert_missing_param_should_not_create_user(client, app):
     res = client.post('/user', data={})
     assert len(res.json['error']) > 0
     assert res.status_code == 400
+    res = client.post('/user', data={
+        'username': 'batima'
+        })
+    assert len(res.json['error']) > 0
+    assert res.status_code == 400
+
+
+def test_insert_existing_user_should_location_to_existing_user(client, app):
+    payload = {
+            'username': 'batima',
+            'password': 'batima123',
+            'email': 'teste@teste.com'
+            }
+    insert_user(payload)
+    res = client.post('/user', data=payload)
+    assert res.status_code == 409
