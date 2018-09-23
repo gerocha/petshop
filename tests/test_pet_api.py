46adb7b04740be1fb2aa0ae2a23ae766be9494ec
Gel <gel@leandro@alertaapp.com.br>
2018-09-22 22:18:59 -0300

    correcting creation test, now its passing

--- tests/test_pet_api.py
+++ tests/test_pet_api.py
@@ -1,12 +1,14 @@
-from flask import jsonify
-
 def test_insert_pet_should_insert(client, logged_batima):
     payload = {
             'name': 'fofinho',
             'type': 'dog',
             }
 
-    res = client.post('/pet', data=payload,
-            headers={'AUTHORIZATION': logged_batima})
+    access_token = 'JWT ' + logged_batima.json['access_token']
+    res = client.post('/pet',
+                      data=payload,
+                      headers={
+                          'AUTHORIZATION': access_token
+                          })
 
     assert res.status_code == 201
