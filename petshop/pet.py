c62c011900321c9d85344dbc44a7e03481145118
Gel <gel@leandro@alertaapp.com.br>
2018-09-22 22:17:38 -0300

    correction o pet api jwt validation

--- petshop/pet.py
+++ petshop/pet.py
@@ -8,6 +8,7 @@ from flask import (
 from sqlalchemy.exc import IntegrityError
 from marshmallow import Schema, fields, ValidationError
 from petshop.pet_model import insert_pet
+from flask_jwt import jwt_required, current_identity
 
 
 class PetSchema(Schema):
@@ -16,10 +17,14 @@ class PetSchema(Schema):
 
 
 class PetAPI(MethodView):
+    decorators = [jwt_required()]
+
     def post(self):
         try:
             pet = PetSchema().load(request.form)
-            insert_pet(pet)
+            payload = pet.copy()
+            payload['user_id'] = current_identity.id
+            insert_pet(payload)
             return jsonify({
                 'data': pet
                 }), 201
