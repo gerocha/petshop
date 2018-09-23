124f9c33a0f97b477382177d60aeafd347f5aa2d
Gel <gel@leandro@alertaapp.com.br>
2018-09-22 22:18:33 -0300

    correction of test payload

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,11 +1,11 @@
 from flask import json
-from petshop.user_model import get_user
 
 
 def test_login_with_right_credentials(user_batima, client):
-    res = client.post('/auth', data=json.dumps(user_batima),
+    res = client.post('/auth',
+                      data=json.dumps({
+                          'username': user_batima['username'],
+                          'password': user_batima['password']
+                          }),
                       headers={'content-type': 'application/json'})
-
-    print(get_user('batima').is_correct_password('123456'))
-
     assert res.status_code == 200
